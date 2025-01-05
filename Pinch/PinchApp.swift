2e14399b89b9932efb4234c15b9a68c28402be51
shouta0715 <kshouta0715@gmail.com>
2025-01-05 15:57:48 +0900

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by k21047kk on 2025/01/05.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
