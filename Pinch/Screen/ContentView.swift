d4d5667e0800d79f264c0c6c3d453625e156e61d
shouta0715 <kshouta0715@gmail.com>
2025-01-05 16:50:43 +0900

    add: gesture

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -8,14 +8,72 @@
 import SwiftUI
 
 struct ContentView: View {
+  // MARK: - PROPERTIES
+  
+  @State private var isAnimating: Bool = false
+  
+  @State private var imageScale: CGFloat = 1.0
+  
+  @State private var imageOffset: CGSize = .zero
+  
+  
+  // MARK: - FUNCTION
+  
+  func resetImageState() {
+    return withAnimation(.spring()) {
+      imageScale = 1
+      imageOffset = .zero
+    }
+  }
+  
+  // MARK: - CONTENT
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+      NavigationView {
+        ZStack {
+          // MARK: - PAGE IMAGE
+          Image(.magazineFrontCover)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .cornerRadius(10)
+            .padding()
+            .shadow(color: .black.opacity((0.2)), radius: 12,x: 2,y: 2)
+            .opacity(isAnimating ? 1 : 0)
+            .offset(imageOffset)
+            .scaleEffect(imageScale)
+          // MARK: - 1. TAP GESTURE
+            .onTapGesture(count: 2, perform:{
+              if imageScale == 1 {
+                withAnimation(.spring()) {
+                  imageScale = 5
+                }
+              } else {
+                resetImageState()
+              }
+            })
+          // MARK: - 2. Drag gesture
+            .gesture(
+              DragGesture()
+                .onChanged({ info in
+                  withAnimation(.linear(duration: 1)) {
+                    imageOffset = info.translation
+                  }
+              })
+                .onEnded { _ in
+                  if imageScale <= 1 {
+                    resetImageState()
+                  }
+                }
+            )
+        } //: ZStack
+        .navigationTitle("Pinch & Zoom")
+        .navigationBarTitleDisplayMode(.inline)
+        .onAppear(perform: {
+          withAnimation(.linear(duration: 1.0)) {
+            isAnimating = true
+          }
+        })
+      } //: Navigation
+      .navigationViewStyle(.stack)
     }
 }
 
